e03a5cba0574c1f8e07a0f0ad6d463686a544c12
Aurelio Gareca <aurelio.gareca.94@hotmail.com>
2024-01-06 19:14:58 -0300

    Se añadió funcionalidad al botón clear e igual. Queda resolver los retos.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Boton from './componentes/Boton';
 import Pantalla from './componentes/Pantalla';
 import BotonClear from './componentes/BotonClear';
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
 
@@ -11,6 +12,14 @@ function App() {
 
   const agregarInput = valor => {
     setInput(input + valor);
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Por favor, ingrese valores para realizar los cálculos.");
+    }
   };
 
   return (
@@ -44,13 +53,13 @@ function App() {
           <Boton manejarClic={agregarInput}>*</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClic={agregarInput}>=</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
           <Boton manejarClic={agregarInput}>0</Boton>
           <Boton manejarClic={agregarInput}>.</Boton>
           <Boton manejarClic={agregarInput}>/</Boton>
         </div>
         <div className='fila'>
-          <BotonClear>Clear</BotonClear>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
         </div>
       </div>
     </div>
